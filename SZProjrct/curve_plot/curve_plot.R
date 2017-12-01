1d493851f20fffe5a687f01229f15b54d008943b
yuyingxie <yuyingxie@gmail.com>
2017-12-01 01:05:23 -0500

    updated code by xyy

--- SZProjrct/curve_plot/curve_plot.R
+++ SZProjrct/curve_plot/curve_plot.R
@@ -69,5 +69,44 @@ for (i in 1:ncol(phe.prof)) {
 }
 
 
+for (i in 1:ncol(phe.prof)) {
+    phe <- phe.prof[, i]
+    names(phe) <- rownames(phe.prof)
+    phe <- sort(phe)
+    motu <- motu.phe.prof[, pmatch(names(phe), colnames(motu.phe.prof))]
+    motu.edit <- ModifymOTU(phe, motu)
+    phe.edit <- as.numeric(names(table(phe)))
+    iplot.phe <- iplotCurves(motu.edit, phe.edit, 
+            chartOpts = list(xlab = colnames(phe.prof)[i], ylab = "Abundance"))
+    iplot.name <- paste(colnames(phe.prof)[i], "html", sep = ".")
+    htmlwidgets::saveWidget(iplot.phe, file = iplot.name)
+}
+
+#heatcols <- heat.colors(100)
+
+pdf('Neg_factor1.pdf', width = 10, height = 4)
+heatcols = colorRampPalette(c("red","white","blue"))(100)
+
+j = 1
+i = 1
+min_val = min(phe.prof[, j])
+max_val = max(phe.prof[, j])
+plot(x = 1:360, y = motu.phe.prof[ ,1], axes = F, type = 'l', col = heatcols[col_value ], ylim = c(0, 1), 
+        ylab = colnames(phe.prof)[1] , xlab = 'motu')
+axis(1)
+axis(2)
+
+for(i in 2:90){
+    col_value = round((phe.prof[i, j] - min_val + 1) / (max_val - min_val) * 100)
+    lines(x = 1:360, y = motu.phe.prof[ ,i],  type = 'l', col = heatcols[col_value ])    
+}
+
+par(fig=c(.85, .9, .8, .98), mai=c(.1 ,.1 ,.3, .1), new=T)
+image(z = 1-rbind(1:length(heatcols)), axes = F, col = heatcols, main = "Weight") #for the legend
+box()
+axis(2 , at = c(0, 0.25, 0.5, 0.75, 1), labels = c("0", round(sqrt(0.25) * max_val, 1), 
+                round(sqrt(0.5) * max_val, 1), round(sqrt(0.75) * max_val, 1) , round(max_val, 1)),las=1)          
+
+dev.off()
 
 
